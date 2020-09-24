da7c6f36e78a3c41349ca06aa66539a209fa2083
isabellajoo <dmdkdmdkdkdk@gmail.com>
2020-09-25 02:11:51 +0900

    image drag and drop

--- web/dragNdrop.js
+++ web/dragNdrop.js
@@ -1,29 +1,105 @@
-/*
+
 
 window.onload = function() {
-    var content = document.getElementsByClassName('content');
-    var impugn = document.getElementById('img-upload');
+    var dropArea = document.getElementById('drop-area');
 
-    content.addEventListener("dragover", function( event ) {
-        event.stopPropagation();
-        event.preventDefault();
+    ;['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+        dropArea.addEventListener(eventName, preventDefaults, false);
+    });
 
-        if(event.type === "dragover") {
-            console.log("dragover");
-            impugn.classList.toggle("dragover");
+    ;['dragenter', 'dragover'].forEach(eventName => {
+        dropArea.addEventListener(eventName, highlight, false);
+    });
+
+    ;['dragleave', 'drop'].forEach(eventName => {
+       dropArea.addEventListener(eventName, unhighlight, false);
+    });
+
+    dropArea.addEventListener('drop', handleDrop, false);
+
+    function preventDefaults(e) {
+        e.preventDefault();
+        e.stopPropagation();
+    }
+
+    function highlight(e) {
+        dropArea.classList.add('highlight');
+    }
+
+    function unhighlight(e) {
+        dropArea.classList.remove('highlight');
+    }
+
+    function handleDrop(e) {
+        let dt = e.dataTransfer;
+        let files = dt.files;
+
+        //handleFiles(files)
+    }
+
+    /*
+    let uploadProgress = []
+    let progressBar = document.getElementById('progress-bar')
+
+    function initializeProgress(numFiles) {
+        progressBar.value = 0;
+        uploadProgress = [];
+
+        for(var i = numFiles; i > 0; i--) {
+            uploadProgress.push(0);
         }
+    }
 
-    }, false);
 
-    content.addEventListener("dragleave", function( event ) {
-        event.stopPropagation();
-        event.preventDefault();
-    }, false);
+    function updateProgress(fileNumber, percent) {
+        uploadProgress[fileNumber] = percent;
+        var total = uploadProgress.reduce((tot, curr) => tot + curr, 0) / uploadProgress.length;
+        console.debug('update', fileNumber, percent, total);
+        progressBar.value = total;
+    }
 
-    content.addEventListener("uploadFiles", function( event ) {
-        event.stopPropagation();
-        event.preventDefault();
-    }, false);
+    function handleFiles(files) {
+        files = [...files];
+        initializeProgress(files, length);
+        //files.forEach(uploadFile);
+        files.forEach(previewFile);
+    }
+
+    function previewFile(file) {
+        var reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onloadend = function() {
+            var img = document.createElement('img');
+            img.src = reader.result;
+            document.getElementById('gallery').appendChild(img);
+        }
+    }
+
+    /*
+    function uploadFile(file, i) {
+        var url = '';
+        var xhr = new XMLHttpRequest();
+        var formData = new FormData();
+        xhr.open('POST', url, true);
+        xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+
+        xhr.upload.addEventListener('progress', function(e) {
+            updateProgress(i, (e.loaded * 100.0 / e.total) || 100);
+        })
+
+        xhr.addEventListener('readystatechange', function(e) {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                updateProgress(i, 100);
+            }
+            else if (xhr.readyState === 4 && xhr.status !== 200) {
+                console.log('Error');
+            }
+        })
+
+        formData.append('upload_preset', 'ujpu6gyk');
+        formData.append('file', file);
+        xhr.send(formData);
+    }
+    */
+
 }
-
- */
