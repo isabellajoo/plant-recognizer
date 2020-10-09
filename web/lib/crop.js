4e0c8c486b3d43dfcfa6aa7e49d48d1cb97b461b
isabellajoo <dmdkdmdkdkdk@gmail.com>
2020-10-09 21:29:07 +0900

    adjusted max height of modal image

--- web/lib/crop.js
+++ web/lib/crop.js
@@ -1,4 +1,4 @@
-window.addEventListener('DOMContentLoaded', function() {
+window.onload = function() {
     var image = document.getElementById('img-preview');
     var cropBoxData;
     var canvasData;
@@ -9,10 +9,20 @@ window.addEventListener('DOMContentLoaded', function() {
             autoCropArea: 0.5,
             ready: function() {
                 cropper.setCropBoxData(cropBoxData).setCanvasData(canvasData);
+            },
+            crop: function(e) {
+                console.log(e.detail.x);
+                console.log(e.detail.y);
+                console.log(e.detail.width);
+                console.log(e.detail.height);
+                console.log(e.detail.rotate);
+                console.log(e.detail.scaleX);
+                console.log(e.detail.scaleY);
             }
         });
     }).on('hidden.bs.modal', function() {
         cropBoxData = cropper.getCropBoxData();
         canvasData = cropper.getCanvasData();
     });
-});
+
+}
