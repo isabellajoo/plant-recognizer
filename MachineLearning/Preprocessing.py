d8f4d074f1af67991eed3446148217f64a82f8c2
Huiseung Son <sonhees99@naver.com>
2020-11-07 11:54:41 +0900

    Update ML

--- MachineLearning/Preprocessing.py
+++ MachineLearning/Preprocessing.py
@@ -1,19 +1,24 @@
 import numpy as np
+import argparse
 import h5py
+from sklearn.model_selection import train_test_split
+
+data_name = 'data.hdf5'
 train_name = 'train.hdf5'
-tr_name = 'train1.hdf5'
 test_name = 'test.hdf5'
-te_name = 'test1.hdf5'
 
-with h5py.File(train_name, 'r') as f1, h5py.File(tr_name, 'w') as f2:
-    f2.create_dataset('X', f1['X'].shape)
-    f2['X'][:10000] = np.array(f1['X'][:10000], dtype=float) / 255
-    f2['X'][10000:] = np.array(f1['X'][10000:], dtype=float) / 255
+with h5py.File(data_name, 'r') as data:
+    X, y = data['X'][:], data['y'][:]
+    X_train, X_test, y_train, y_test = train_test_split(X, y)
+    with h5py.File(train_name, 'w') as train:
+        train.create_dataset('X', X_train.shape)
+        train['X'][:10000] = np.array(X_train[:10000], dtype=float) / 255
+        train['X'][10000:] = np.array(X_train[10000:], dtype=float) / 255
     
-    f2.create_dataset('y', f1['y'].shape, data=f1['y'])
+        train.create_dataset('y', y_train.shape, data=y_train)
 
-with h5py.File(test_name, 'r') as f1, h5py.File(te_name, 'w') as f2:
-    X = np.array(f1['X'], dtype=float) / 255
-    f2.create_dataset('X', X.shape, data=X)
-    
-    f2.create_dataset('y', f1['y'].shape, data=f1['y'])
+    with h5py.File(test_name, 'w') as test:
+        _X = np.array(X_test, dtype=float) / 255
+        test.create_dataset('X', _X.shape, data=_X)
+        
+        test.create_dataset('y', y_test.shape, data=y_test)
