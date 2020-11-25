18cb4bb3cf698b50ce6e990e93e7d15822991156
isabellajoo <dmdkdmdkdkdk@gmail.com>
2020-11-25 11:43:11 +0000

    changed port number

--- web/server.js
+++ web/server.js
@@ -13,8 +13,8 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
-const server = app.listen(3000, function() {
-    console.log("Express server has started on port 3000");
+const server = app.listen(80, function() {
+    console.log("Express server has started on port 80");
 });
 
 app.get('/', function(req, res){
