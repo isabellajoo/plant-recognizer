1c6c89efcbaabe2116f371790057357757a377eb
isabellajoo <dmdkdmdkdkdk@gmail.com>
2020-11-27 10:21:05 +0000

    modified result order

--- web/public/js/result.js
+++ web/public/js/result.js
@@ -3,29 +3,43 @@ window.addEventListener('load', function() {
         document.getElementById('img-search').src = window.localStorage.getItem("imgcropped");
     }
 
-    var resName = document.getElementsByClassName('result-name')[0].innerText;
-    var imgResult = document.getElementsByClassName('img-result');
-    for (var i = 0; i < 5; i++) {
-        imgResult[i].src = "image/" + resName + " 식물/google_000"+ i +".jpg"
-    }
-
     if (localStorage.getItem("resultName") !== null) {
-        var imgOther = document.getElementsByClassName('img-other-result');
         var resultName = document.getElementsByClassName('result-name');
-        var array = JSON.parse(localStorage.getItem("resultName"));
+        var takeName = JSON.parse(localStorage.getItem("resultName"));
+        var takeIndex = JSON.parse(localStorage.getItem("classIndex"));
+        var takeProb = JSON.parse(localStorage.getItem("probability"));
+        console.log(takeName);
+        console.log(takeIndex);
+        console.log(takeProb);
 
-        for (var i = 1; i < array.length; i++) {
-            resultName[i].innerHTML = array[i];
-            imgOther[i - 1].src = "image/" + array[i] + " 식물/google_0000.jpg";
+        var arrayName = [];
+        var arrayIndex = [];
+        var arrayProb = [];
+        for (var i = 0; i < takeName.length; i++) {
+            if (resultName[0].innerText !== takeName[i]) {
+                arrayName.push(takeName[i]);
+                arrayIndex.push(takeIndex[i]);
+                arrayProb.push(takeProb[i]);
+            }
         }
-    }
+        console.log(arrayName);
+        console.log(arrayIndex);
 
-    if (localStorage.getItem("classIndex") !== null && localStorage.getItem("probability") !== null) {
-        var resultHREF = document.getElementsByClassName('result-other-href');
-        var arrayIndex = JSON.parse(localStorage.getItem("classIndex"));
-        var arrayProb = JSON.parse(localStorage.getItem("probability"));
-        for (var i = 1; i < arrayIndex.length; i++) {
-            resultHREF[i - 1].setAttribute('href', '/result?id=' + arrayIndex[i] + '&prob=' + arrayProb[i]);
+        var imgResult = document.getElementsByClassName('img-result');
+        for (var i = 0; i < 5; i++) {
+            imgResult[i].src = "image/" + resultName[0].innerText + " 식물/google_000"+ i +".jpg"
+        }
+
+        for (var i = 5; i < imgResult.length; i++) {
+            resultName[i - 4].innerHTML = arrayName[i - 5];
+            imgResult[i].src = "image/" + arrayName[i - 5] + " 식물/google_0000.jpg";
+        }
+
+        if (localStorage.getItem("classIndex") !== null && localStorage.getItem("probability") !== null) {
+            var resultHREF = document.getElementsByClassName('result-other-href');
+            for (var i = 0; i < arrayIndex.length; i++) {
+                resultHREF[i].setAttribute('href', '/result?id=' + arrayIndex[i] + '&prob=' + arrayProb[i]);
+            }
         }
     }
 
